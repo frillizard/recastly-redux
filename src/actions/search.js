25661ec05712656716155ff7ae66ac8530519075
Rikki Sasda <rikkisasda@gmail.com>
2018-10-04 08:25:51 -0500

    search action implemented

--- src/actions/search.js
+++ src/actions/search.js
@@ -3,10 +3,13 @@ import changeVideoList from './videoList.js';
 import changeVideo from './currentVideo.js';
 import YOUTUBE_API_KEY from '../config/youtube.js';
 
-
 var handleVideoSearch = (q) => {
- 
-  //TODO:  Write an asynchronous action to handle a video search!
+  return () => { 
+    searchYouTube({YOUTUBE_API_KEY, q}, (data) => {
+      changeVideoList(data);
+      changeVideo(data[0]);
+    });
+  };
 };
 
 export default handleVideoSearch;
